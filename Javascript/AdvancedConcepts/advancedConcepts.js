672a4e6f443b3dc9ed45c1127c86e5498daf6152
lokesh2498 <lokesh.padmanabhan@paytm.com>
2024-07-08 15:33:03 +0530

    learning advance concepts of js

--- Javascript/AdvancedConcepts/advancedConcepts.js
+++ Javascript/AdvancedConcepts/advancedConcepts.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /* currying can be created using two ways - closures and bind method */
 
 // bind
@@ -61,26 +63,105 @@ multiplyFour(10);
 
 // SetTimeout + closures
 // using let
-function x() {
-  for (let i = 1; i <= 5; i++) {
-    setTimeout(() => {
-      console.log(i);
-    }, i * 1000);
-  }
-}
+// function x() {
+//   for (let i = 1; i <= 5; i++) {
+//     setTimeout(() => {
+//       console.log(i);
+//     }, i * 1000);
+//   }
+// }
 
-x();
+// x();
 
 // using var
-function y() {
-  for (var i = 1; i <= 5; i++) {
-    function close(i) {
-      setTimeout(() => {
-        console.log(i);
-      }, i * 1000);
-    }
-    close(i);
-  }
+// function y() {
+//   for (var i = 1; i <= 5; i++) {
+//     function close(i) {
+//       setTimeout(() => {
+//         console.log(i);
+//       }, i * 1000);
+//     }
+//     close(i);
+//   }
+// }
+
+// y();
+
+// Callback functions
+setTimeout(() => {
+  console.log('timer');
+}, 300);
+
+function one(two) {
+  console.log('x');
+  two();
 }
 
-y();
+one(function two() {
+  console.log('y');
+});
+
+// event listeners with closures
+
+function addEventListeners() {
+  let count = 0;
+  document
+    .getElementById('click')
+    .addEventListener('click', function () {
+      console.log('Button Clicked', ++count);
+    });
+}
+
+addEventListeners();
+
+// Promises
+const carts = ['shoe', 'cart'];
+const validateCart = (cart) => cart.length > 0;
+
+// Creating a promise (Producer)
+function createCart(carts) {
+  const pr = new Promise(function (resolve, reject) {
+    if (!validateCart(carts)) {
+      const err = new Error('there are no items in the cart');
+      reject(err);
+    }
+    const orderId = '1234';
+    if (orderId) {
+      resolve(orderId);
+    }
+  });
+  return pr;
+}
+
+const proceedToPayment = (orderId) => {
+  return new Promise((resolve, reject) => {
+    if (orderId) {
+      resolve('Payment successful');
+    } else {
+      reject('Payment is Invalid');
+    }
+  });
+};
+
+const showOrderSummary = (paymentInfo) => {
+  return new Promise((resolve, reject) => {
+    const walletBalance = '500 Rs.';
+    if (walletBalance) {
+      resolve(walletBalance);
+    } else {
+      reject('issue with summary');
+    }
+  });
+};
+
+const updateWallet = (walletBalance) => {
+  return `The balance present is ${walletBalance}`;
+};
+
+// Consuming promise (Consumer) Promise chaining
+createCart(carts)
+  .then((orderId) => orderId)
+  .then((orderId) => proceedToPayment(orderId))
+  .then((paymentInfo) => showOrderSummary(paymentInfo))
+  .then((walletBalance) => updateWallet(walletBalance))
+  .catch((err) => console.log('err', err.message));
